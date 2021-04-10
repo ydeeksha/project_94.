22296ae23a1b8340b287309770531849d9eba19b
ydeeksha <75473851+ydeeksha@users.noreply.github.com>
2021-04-10 15:48:04 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD6ABB_cuiNix1n70KeFo_0pSxW8883gCA",
+    authDomain: "kwitter-c9cd0.firebaseapp.com",
+    databaseURL: "https://kwitter-c9cd0-default-rtdb.firebaseio.com",
+    projectId: "kwitter-c9cd0",
+    storageBucket: "kwitter-c9cd0.appspot.com",
+    messagingSenderId: "400267025965",
+    appId: "1:400267025965:web:1ffe3c3ab157f2d9e3e2a4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
